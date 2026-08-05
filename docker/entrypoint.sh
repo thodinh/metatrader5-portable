9bc778ebf9bb996b8c33f415bae62e24be08809e
thodinh <tho.dinh@inno.ai.vn>
2026-08-05 14:47:51 +0000

    Update GitHub workflow and add entrypoint.sh for new build

---
+++ docker/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+rm -f /tmp/.X0-lock
+rm -f /tmp/.X11-unix/X0 2>/dev/null
+
+echo "Starting Xvfb on :0..."
+Xvfb :0 -screen 0 1024x768x24 &
+sleep 2
+
+MT5_COMMON="/opt/wineprefix/drive_c/Metatrader-5/Config/common.ini"
+if [ -f "$MT5_COMMON" ]; then
+    wine python -c "
+import sys
+data = open('$MT5_COMMON', 'rb').read()
+patched = data.replace(b'E\x00n\x00a\x00b\x00l\x00e\x00d\x00=\x000\x00', b'E\x00n\x00a\x00b\x00l\x00e\x00d\x00=\x001\x00', 1)
+if patched != data:
+    open('$MT5_COMMON', 'wb').write(patched)
+"
+fi
+
+exec "$@"
